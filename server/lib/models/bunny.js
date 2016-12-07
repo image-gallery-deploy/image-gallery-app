99d3196470790149f53519a03b1a71173059a7d5
Nathan P. Keene <npk@txtincorporated.com>
2016-12-07 15:56:35 -0800

    Working on API for bunnies

---
+++ server/lib/models/bunny.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  }
+});
+
+module.exports = mongoose.model('Bunny', schema);
