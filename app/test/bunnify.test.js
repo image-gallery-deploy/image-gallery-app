9eef93d2b7b3d4d590f441e286fcf84d3473e06d
Nathan P. Keene <npk@txtincorporated.com>
2016-12-12 15:26:32 -0800

    bunny-big.test.js passing

--- app/test/bunnify.test.js
+++ app/test/bunnify.test.js
@@ -14,20 +14,6 @@ describe('test UI add component', () => {
 
   describe('calls bunny-app\'s add() fn', () => {
 
-    const bunnies = [
-      { 
-        title: 'Snuggly-wuggly', 
-        url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
-        description: 'Aren\'t I the little dickens!' 
-      },
-      { 
-        title: 'Wudgy-wudgy', 
-        url: 'http://cdn.wallpapersafari.com/48/78/wf3rOl.jpg', 
-        description: 'Wiffle-wiffle!' 
-      }
-
-    ];
-
     const bunny = { 
       title: 'Ooooooh!', 
       url: 'https://media.yayomg.com/wp-content/uploads/2014/04/yayomg-tiny-bunny.png', 
@@ -35,7 +21,6 @@ describe('test UI add component', () => {
     };
 
     const bunnyApp = {
-
       add(bunny) {
         return Promise.resolve( bunny );
       }
@@ -51,11 +36,9 @@ describe('test UI add component', () => {
 
       setTimeout(() => {
         assert.deepEqual( component.add(), bunny );
-        // assert.equal( bunnies[2], bunny );
         done();
-      });
-      
-    });
 
+      });      
+    });
   });
 });
--- app/test/bunny-big.test.js
+++ app/test/bunny-big.test.js
@@ -1,88 +1,45 @@
 
-// describe('test main component', () => {
+describe('test UI add component', () => {
 
-//   const { assert } = chai;
+  const { assert } = chai;
 
-//   beforeEach(
-//     angular.mock.module('components')
-//   );
+  beforeEach(
+    angular.mock.module('components')
+  );
 
-//   let $component = null;
-//   beforeEach(angular.mock.inject($componentController => {
-//     $component = $componentController;
-//   }));
+  let $component = null;
+  beforeEach(angular.mock.inject($componentController => {
+    $component = $componentController;
+  }));
 
-//   describe('calls bunny-big\'s remove() fn', () => {
+  describe('calls bunny-app\'s add() fn', () => {
 
-//     const bunnies = [
-//       { 
-//         title: 'Snuggly-wuggly', 
-//         url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
-//         description: 'Aren\'t I the little dickens!' 
-//       },
-//       { 
-//         title: 'Wudgy-wudgy', 
-//         url: 'http://cdn.wallpapersafari.com/48/78/wf3rOl.jpg', 
-//         description: 'Wiffle-wiffle!' 
-//       }
+    const bunny = { 
+      title: 'Ooooooh!', 
+      url: 'https://media.yayomg.com/wp-content/uploads/2014/04/yayomg-tiny-bunny.png', 
+      description: 'Meep!' 
+    };
 
-//     ];
+    const bunnyApp = {
 
-//     const bunny = { 
-//       title: 'Ooooooh!', 
-//       url: 'https://media.yayomg.com/wp-content/uploads/2014/04/yayomg-tiny-bunny.png', 
-//       description: 'Meep!' 
-//     };
+      remove(bunny) {
+        return Promise.resolve( bunny );
+      }
 
-//     const bunnyService = {
-//       get() {
-//         return Promise.resolve( bunnies );
-//       },
-//       add(bunny) {
-//         return Promise.resolve( bunny );
-//       },
-//       remove() {
-//         return Promise.resolve( bunny );
-//       }
+    };
 
-//     };
-
-    
-//     it('loads bunnies', done => {
-//       const component = $component('bunnyApp', { bunnyService });
-
-//       setTimeout(() => {
-//         assert.equal(component.bunnies, bunnies);
-//         done();
-//       });
-
-//     });
-
-//     it('adds bunnies', done => {
-//       const component = $component('bunnyApp', { bunnyService });
-
-//       component.add( bunny );
-
-//       setTimeout(() => {
-//         assert.equal( bunnies.length, 3 );
-//         assert.equal( bunnies[2], bunny );
-//         done();
-//       });
+    it('removes bunnies', done => {
+      const component = $component('bunnyBig', { bunnyApp });
       
-//     });
+      component.remove = () => bunny;
 
-//     it('removes bunnies', done => {
-//       const component = $component('bunnyApp', { bunnyService });
-      
-//       component.remove( bunny );
+      component.delete( bunny );
 
-//       setTimeout(() => {
-//         assert.equal( bunnies.length, 2 );
-//         assert.equal( bunnies.indexOf( bunny ), -1 );
-//         done();
+      setTimeout(() => {
+        assert.deepEqual( component.remove(), bunny );
+        done();
 
-//       });
-
-//     });
-//   });
-// });
+      });      
+    });
+  });
+});
