deb7782c83da479dd52293538163484d6a4a0bf6
Nathan P. Keene <npk@txtincorporated.com>
2016-12-12 15:01:13 -0800

    bunnify.test.js working

--- app/test/bunnify.test.js
+++ app/test/bunnify.test.js
@@ -1,61 +1,61 @@
 
-// describe('test UI add component', () => {
+describe('test UI add component', () => {
 
-//   const { assert } = chai;
+  const { assert } = chai;
 
-//   beforeEach(
-//     angular.mock.module('components')
-//   );
+  beforeEach(
+    angular.mock.module('components')
+  );
 
-//   let $component = null;
-//   beforeEach(angular.mock.inject($componentController => {
-//     $component = $componentController;
-//   }));
+  let $component = null;
+  beforeEach(angular.mock.inject($componentController => {
+    $component = $componentController;
+  }));
 
-//   describe('calls bunny-app\'s add() fn', () => {
+  describe('calls bunny-app\'s add() fn', () => {
 
-//     const bunnies = [
-//       { 
-//         title: 'Snuggly-wuggly', 
-//         url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
-//         description: 'Aren\'t I the little dickens!' 
-//       },
-//       { 
-//         title: 'Wudgy-wudgy', 
-//         url: 'http://cdn.wallpapersafari.com/48/78/wf3rOl.jpg', 
-//         description: 'Wiffle-wiffle!' 
-//       }
+    const bunnies = [
+      { 
+        title: 'Snuggly-wuggly', 
+        url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
+        description: 'Aren\'t I the little dickens!' 
+      },
+      { 
+        title: 'Wudgy-wudgy', 
+        url: 'http://cdn.wallpapersafari.com/48/78/wf3rOl.jpg', 
+        description: 'Wiffle-wiffle!' 
+      }
 
-//     ];
+    ];
 
-//     const bunny = { 
-//       title: 'Ooooooh!', 
-//       url: 'https://media.yayomg.com/wp-content/uploads/2014/04/yayomg-tiny-bunny.png', 
-//       description: 'Meep!' 
-//     };
+    const bunny = { 
+      title: 'Ooooooh!', 
+      url: 'https://media.yayomg.com/wp-content/uploads/2014/04/yayomg-tiny-bunny.png', 
+      description: 'Meep!' 
+    };
 
-//     const bunnyApp = {
-//       get() {
-//         return Promise.resolve( bunnies );
-//       },    
-//       add(bunny) {
-//         return Promise.resolve( bunny );
-//       }
+    const bunnyApp = {
 
-//     };
+      add(bunny) {
+        return Promise.resolve( bunny );
+      }
 
-//     it('adds bunnies', done => {
-//       const component = $component('bunnify', { bunnyApp });
+    };
 
-//       component.addBunny( bunny );
-
-//       setTimeout(() => {
-//         assert.equal( bunnies.length, 3 );
-//         assert.equal( bunnies[2], bunny );
-//         done();
-//       });
+    it('adds bunnies', done => {
+      const component = $component('bunnify', { bunnyApp });
       
-//     });
+      component.add = () => bunny;
 
-//   });
-// });
+      component.addBunny( bunny );
+
+      setTimeout(() => {
+        assert.deepEqual( component.add(), bunny );
+        // assert.equal( bunnies[2], bunny );
+        done();
+      });
+      
+    });
+
+  });
+});
