da1ec43b9868d85d03ef7eae33572daa8d9a52c4
Nathan P. Keene <npk@txtincorporated.com>
2017-01-17 15:48:57 -0800

    Set API_URL

--- src/main.js
+++ src/main.js
@@ -17,6 +17,6 @@ const app = angular.module('myApp', [
 
 app.config(routes);
 
-const dev = 'http://localhost:3000/api';
+const dev = process.env.API_URL || 'http://localhost:3000/api';
 
 app.value('apiUrl', dev);
