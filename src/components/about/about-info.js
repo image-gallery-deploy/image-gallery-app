c82abf9f6411a7ff6253e7b078696e0ac0782c28
Nathan P. Keene <npk@txtincorporated.com>
2017-01-19 10:02:51 -0800

    Fill out About view

--- src/components/about/about-info.js
+++ src/components/about/about-info.js
@@ -1,4 +1,8 @@
 export default {
   template: `
-<p>All kritters karefully kurated from the 'World Wide Web'</p>`
+<p>This is the deployed version of a six-part lab exploring Angular and Angular-UI-Router in a Webpack build environment and with some light styling using SCSS.  <p>
+</p>It is deployed to Heroku using continuous push from the GitHub repo and provisioned on the backend with Heroku's MongoDB integration from mLab (see 'Contact' page for GitHub link).</p>
+<p>Feel free to add and subtract albums and images at will.  <p>
+</p>See 'Contact' page for link to source code and sample data.</p>
+`
 };
