88976142931c59cb70178347475fd793cc5fc296
Nathan P. Keene <npk@txtincorporated.com>
2017-01-19 10:10:38 -0800

    Minor edit

--- src/components/about/about-info.js
+++ src/components/about/about-info.js
@@ -1,7 +1,7 @@
 export default {
   template: `
 <p>This is the deployed version of a six-part lab exploring Angular and Angular-UI-Router in a Webpack build environment and with some light styling using SCSS.  <p>
-</p>It is deployed to Heroku using continuous push from the GitHub repo and provisioned on the backend with Heroku's MongoDB integration from mLab (see 'Contact' page for GitHub link).</p>
+</p>It is deployed to Heroku continuously from the GitHub repo and provisioned on the backend with Heroku's MongoDB integration from mLab (see 'Contact' page for GitHub link).</p>
 <p>Feel free to add and subtract albums and images at will.  <p>
 </p>See 'Contact' page for link to source code and sample data.</p>
 `
